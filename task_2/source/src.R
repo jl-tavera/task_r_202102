1fb5aab4252bbd3b91faab639a964d29caff16ef
jl-tavera <jl.tavera@uniandes.edu.co>
2021-11-14 02:20:57 -0500

    Avances

--- task_2/source/src.R
+++ task_2/source/src.R
@@ -10,23 +10,39 @@
 # Taller A #
 #==========#
 
-# librerias
+# Configuración Inicial 
+
 rm(list=ls())
 require(pacman)
 p_load(rio,tidyverse)
 
-# punto 1
+
+#===============#
+# Punto 1 Loops #
+#===============#
+
+#1.0. Crear lista: Cree un objeto tipo lista vacío, llámelo chip.
+
+chip = list()
+
+#1.1. Importar datos: Use un loop para importar cada archivo .xlsx de data/imput
+# en un elemento/posición diferente de chip.
+
 paths = lapply(2017:2020 , function(x) list.files(paste0("task_2/data/imput/",x),full.names=T)) %>%
         unlist()
 
-list_chip = list()
+
 for (i in 1:length(paths)){
-     list_chip[[i]] = import(file = paths[i])  
+     chip[[i]] = import(file = paths[i])  
 }
 
+#====================#
+# Punto 2 Funciones  #
+#====================#
 
-  
-# punto 2
+#2.0. Crear función: Cree una función que extraiga de un dataframe 
+# dentro de chip, el valor PAGOS(Pesos) para la categoría EDUCACION. 
+
 f_extrac = function(n,lista,tipo_rubro){
   lista_n = lista[[n]] 
   colnames(lista_n) = lista_n[7,]
@@ -56,6 +72,15 @@ f_extrac = function(n,lista,tipo_rubro){
 return(df)  
 }
 f_extrac(n = 10 , lista = list_chip , tipo_rubro = "SALUD")
+
+#========================#
+# Punto 3 Familia Apply  #
+#========================#
+
+# 3.0. Aplique la función creada en el punto anterior a todos los elementos 
+# de la lista chip.
+
+
 
 
 
